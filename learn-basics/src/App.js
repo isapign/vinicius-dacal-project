a5f3e84632db0bf90f353e721a62cba665e7abca
Isabelly Pignatari <isabelly.pignatari@e-deploy.com.br>
2021-02-14 17:28:25 -0300

    Transforma class component em hooks

---
+++ learn-basics/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+  const [ showImage, setShowImage ] = useState(false)
+ 
+  const toggle = () => {
+    setShowImage(!showImage)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {showImage && 
+          <img src={logo} className="App-logo" alt="logo" />
+        }
+        <button 
+          type="button" 
+          onClick={toggle}>
+            {showImage ? 'Esconder': 'Mostrar'}
+        </button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a 
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
